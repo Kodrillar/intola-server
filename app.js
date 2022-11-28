a178f04eeb109cf41a4324923d8c69a3a1481920
Kodrillar <ipadeoladavid112@gmail.com>
2022-11-28 21:44:58 +0100

    use in-built http server

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 require("dotenv").config()
 const express = require("express");
 const app = express();
+const http = require("http")
 const cors = require("cors");
 const helmet = require("helmet");
 const morgan = require("morgan");
 const fileupload = require("express-fileupload");
+const server = http.createServer(app);
+
+
 app.use(cors());
 app.use(fileupload());
 app.use(helmet());
@@ -15,7 +19,7 @@ require("./start/logger")();
 require('./start/routes')(app);
 
 const port =process.env.PORT || 4000;
-const server = app.listen(port, ()=>{
+server.listen(port, ()=>{
     console.log("connected on PORT: " + port);
 })
 
